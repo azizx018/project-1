f36781cd4e4beccfea44642ee219cb510909373d
Aziz, Chiara <chiara_aziz@optum.com>
2022-10-04 10:44:26 -0500

    added credential and note service

---
+++ starter/cloudstorage/src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/NoteListService.java
@@ -0,0 +1,38 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.NoteMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.Note;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class NoteListService {
+    private final NoteMapper noteMapper;
+
+    public NoteListService(NoteMapper noteMapper) {
+        this.noteMapper = noteMapper;
+    }
+
+    public int updateNote(Note note) {
+        note.setNoteId(note.getNoteId());
+        note.setNoteDescription(note.getNoteDescription());
+        note.setNoteTitle(note.getNoteTitle());
+        return noteMapper.updateNote(note);
+    }
+
+    public int saveNote(Note note, Integer userId) {
+        return noteMapper.saveNote(new Note(null, note.getNoteTitle(), note.getNoteDescription(), note.getUserId()));
+    }
+    public Note getSelectedNote(Integer noteId) {
+        return noteMapper.selectNote(noteId);
+    }
+
+    public List<Note> getNotesByUser(Integer userId) {
+        return noteMapper.getNotesByUser(userId);
+    }
+
+    public int deleteNote(Integer noteId) {
+        return noteMapper.deleteNoteByNoteId(noteId);
+    }
+}
